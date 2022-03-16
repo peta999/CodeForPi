3f77cbf3f5a5cfd59ccec2d71011bc09b01c94de
peta999 <101709235+peta999@users.noreply.github.com>
2022-03-16 17:10:07 +0100

    client.loop_start()

--- test.py
+++ test.py
@@ -40,7 +40,9 @@ def main():
 #        return -1
     client.on_connect = on_connect
     client.on_message = on_message
+    client.on_disconnect = on_disconnect
     client.connect("192.168.2.54", 1883, 60)
+    client.loop_start()
     count = 0   # count to keep cooldown after sending warning email
     while True:
         temp, hum = get_temperature_humidity()
@@ -56,7 +58,6 @@ def main():
             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
             data = "temperature: {}, humidity: {}, date: {}".format(temp, hum, dt_string)
             client.publish("data", data, 1)
-            client.on_disconnect = on_disconnect
             print(str(data))
 
             if(len(temp_list) > 0 and sum(temp_list) / len(temp_list) <= 9.5 and count == 0):
@@ -77,8 +78,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("$SYS/#")
 
 def on_disconnect(client, userdata, rc):
-    if rc != 0:
-        print("Unexpected disconnection")
+    print("Unexpected disconnection")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
