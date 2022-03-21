21eeaa053a0a4c09a0e83cd69d558a6f11157f54
peta999 <101709235+peta999@users.noreply.github.com>
2022-03-21 11:49:03 +0100

    1

--- test.py
+++ test.py
@@ -207,12 +207,10 @@ def updateConfig():
         # The FullLoader parameter handles the conversion from YAML
         # scalar values to Python the dictionary format
         list_doc = yaml.safe_load(file)
-        print(list_doc)
-        list_doc["huehnerstall"]["verschiebung_abends"] = 5
-        print(list_doc["huehnerstall"]["verschiebung_abends"])
 
     list_doc["huehnerstall"]["verschiebung_abends"] = verschiebung_abends
     list_doc["huehnerstall"]["verschiebung_morgens"] = verschiebung_morgens
+    print(list_doc)
 
     with open("config.yaml", "w") as f:
         yaml.dump(list_doc, f)
